80f404ecaf4fa5270b0ad8fcf616eaa29ac812b2
juniorconde <rubenscondejr@uol.com.br>
2024-09-25 06:15:50 -0300

    wip(primeiras aplicações e interações)

---
+++ GestãoEstoque/main.rb
@@ -0,0 +1,8 @@
+# main.rb
+require_relative 'menu'
+require_relative 'estoque'
+require_relative 'produto'
+require_relative 'venda'
+
+# Inicia o sistema de gerenciamento de estoque
+Menu.new
---
+++ GestãoEstoque/vendas.rb
@@ -0,0 +1,26 @@
+class Venda
+  def initialize(estoque)
+    @estoque = estoque
+  end
+
+  def registrar_venda
+    puts "Código do produto:"
+    codigo = gets.chomp
+    produto = @estoque.encontrar_produto(codigo)
+
+    if produto
+      puts "Quantidade a vender:"
+      quantidade_venda = gets.chomp.to_i
+
+      if produto[:quantidade] >= quantidade_venda
+        produto[:quantidade] -= quantidade_venda
+        @estoque.salvar_estoque
+        puts "Venda registrada com sucesso!"
+      else
+        puts "Quantidade insuficiente no estoque."
+      end
+    else
+      puts "Produto não encontrado."
+    end
+  end
+end
