b306b366744f3498d128deaea4050777366e7c2c
juniorconde <rubenscondejr@uol.com.br>
2024-09-27 07:08:38 -0300

    refactor(alteração codigo, estancia, json)

--- GestaoEstoque/produto.rb
+++ GestaoEstoque/produto.rb
@@ -1,6 +1,8 @@
 class Produto
+  # Define getter e setter para todos os atributos do produto.
   attr_accessor :codigo, :descricao, :fornecedor, :data_compra, :valor_compra, :quantidade, :categoria
 
+  # Inicializa um objeto Produto com os atributos fornecidos.
   def initialize(codigo, descricao, fornecedor, data_compra, valor_compra, quantidade, categoria)
     @codigo = codigo
     @descricao = descricao
@@ -8,10 +10,24 @@ class Produto
     @data_compra = data_compra
     @valor_compra = valor_compra
     @quantidade = quantidade
-    @categoria = categoria  # Novo atributo adicionado aqui
+    @categoria = categoria  # Inicializa o atributo categoria
   end
 
+  # Converte o objeto Produto em um hash para ser salvo em JSON.
   def to_h
+    {
+      codigo: @codigo,
+      descricao: @descricao,
+      fornecedor: @fornecedor,
+      data_compra: @data_compra,
+      valor_compra: @valor_compra,
+      quantidade: @quantidade,
+      categoria: @categoria
+    }
+  end
+
+  # Método adicional para exibir as informações do produto.
+  def exibir_detalhes
     puts "Código: #{@codigo}"
     puts "Descrição: #{@descricao}"
     puts "Fornecedor: #{@fornecedor}"
